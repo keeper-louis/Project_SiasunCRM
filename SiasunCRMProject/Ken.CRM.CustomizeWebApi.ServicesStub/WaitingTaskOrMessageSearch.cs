3c5f086706e4963d8b2e8b09a6d6db9786781eb1
keeper-louis <keeper_louis@sina.com>
2019-10-24 13:38:58 +0800

    tij

--- SiasunCRMProject/Ken.CRM.CustomizeWebApi.ServicesStub/WaitingTaskOrMessageSearch.cs
+++ SiasunCRMProject/Ken.CRM.CustomizeWebApi.ServicesStub/WaitingTaskOrMessageSearch.cs
@@ -57,7 +57,7 @@ namespace Ken.CRM.CustomizeWebApi.ServicesStub
 		                                    AND t_WF_ApprovalItem.FReceiverId = t_WF_Receiver.FReceiverId) 
 											left join T_SEC_USER T_SEC_USER1 on T_SEC_USER1.FUSERID=t_WF_ProcInst.FORIGINATORID 
 											left join T_WF_PROCDEF_L on T_WF_PROCDEF_L.FPROCDEFID=t_WF_ProcInst.FPROCDEFID
- where t_WF_Assign.FSTATUS=0 and  t_WF_Receiver.FReceiverId='{0}' order by t_WF_ApprovalAssign.FDEALTIME desc", FRECEIVERID);
+ where t_WF_Assign.FSTATUS=0 and  t_WF_ProcInst.Fstatus = 2 and t_WF_Receiver.FReceiverId='{0}' order by t_WF_ApprovalAssign.FDEALTIME desc", FRECEIVERID);
                     DynamicObjectCollection items = DBUtils.ExecuteDynamicObject(ctx, strSql);
                     foreach (DynamicObject item in items)
                     {
