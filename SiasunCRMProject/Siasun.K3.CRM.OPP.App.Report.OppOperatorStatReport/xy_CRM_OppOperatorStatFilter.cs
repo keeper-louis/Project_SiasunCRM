0b6ea720437995f93ae97fe569b59db551a175c4
lcdoit <39624646+lcdoit@users.noreply.github.com>
2019-11-12 09:57:16 +0800

    提交 修改过滤 部门隔离

--- SiasunCRMProject/Siasun.K3.CRM.OPP.App.Report.OppOperatorStatReport/xy_CRM_OppOperatorStatFilter.cs
+++ SiasunCRMProject/Siasun.K3.CRM.OPP.App.Report.OppOperatorStatReport/xy_CRM_OppOperatorStatFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Kingdee.BOS;
 using Kingdee.BOS.Core.CommonFilter.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
 using Kingdee.BOS.Core.DynamicForm.PlugIn.ControlModel;
 using Kingdee.BOS.Core.Metadata;
 using System;
@@ -65,5 +66,16 @@ namespace Siasun.K3.CRM.OPP.App.Report.OppOperatorStatReport
             comFieldEditor3.SetComboItems(lstBillStatusItems);
             this.View.Model.SetValue("F_xy_BillStatus", "C");            
         }
+        public override void BeforeF7Select(BeforeF7SelectEventArgs e)
+        {
+            base.BeforeF7Select(e);
+        }
+        public override void AuthPermissionBeforeF7Select(AuthPermissionBeforeF7SelectEventArgs e)
+        {
+            base.AuthPermissionBeforeF7Select(e);
+            e.IsIsolationOrg = false;//是否进行组织隔离
+        }
     }
+
+
 }
