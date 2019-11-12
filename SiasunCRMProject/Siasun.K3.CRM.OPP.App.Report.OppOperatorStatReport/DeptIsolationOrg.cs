f8333db3f16e59633f774192064ca3b2bfbfcf9e
lcdoit <39624646+lcdoit@users.noreply.github.com>
2019-11-12 11:20:56 +0800

    提交 添加去掉组织隔离插件

---
+++ SiasunCRMProject/Siasun.K3.CRM.OPP.App.Report.OppOperatorStatReport/DeptIsolationOrg.cs
@@ -0,0 +1,23 @@
+﻿using Kingdee.BOS;
+using Kingdee.BOS.Core.CommonFilter.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.ControlModel;
+using Kingdee.BOS.Core.Metadata;
+using System;
+using System.Collections.Generic;
+
+namespace Siasun.K3.CRM.OPP.App.Report.OppOperatorStatReport
+{
+  public   class DeptIsolationOrg : AbstractCommonFilterPlugIn
+    {
+        public override void BeforeF7Select(BeforeF7SelectEventArgs e)
+        {
+            base.BeforeF7Select(e);
+        }
+        public override void AuthPermissionBeforeF7Select(AuthPermissionBeforeF7SelectEventArgs e)
+        {
+            base.AuthPermissionBeforeF7Select(e);
+            e.IsIsolationOrg = false;//是否进行组织隔离
+        }
+    }
+}
