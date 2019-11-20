b04cb148495b360e278137bc6c74aad945e5e1a9
lcdoit <39624646+lcdoit@users.noreply.github.com>
2019-11-20 20:36:46 +0800

    提交 添加 获取CRM 角色

--- SiasunCRMProject/Ken.CRM.CustomizeWebApi.ServicesStub/CRMService.cs
+++ SiasunCRMProject/Ken.CRM.CustomizeWebApi.ServicesStub/CRMService.cs
@@ -1,5 +1,7 @@
 ﻿using Kingdee.BOS;
+using Kingdee.BOS.App.Data;
 using Kingdee.BOS.Authentication;
+using Kingdee.BOS.Orm.DataEntity;
 using Kingdee.BOS.ServiceFacade.KDServiceClient.User;
 using Kingdee.BOS.ServiceFacade.KDServiceFx;
 using Kingdee.BOS.WebApi.Client;
@@ -41,18 +43,25 @@ namespace Ken.CRM.CustomizeWebApi.ServicesStub
             ApiClient client = new ApiClient("http://localhost/K3Cloud/");
             bool bLogin = client.Login(DBID, UserName, PassWord, 2052);
             if (bLogin)//登录成功
-            {
-                if (SearchType.Equals("0"))
+            {   if (getCrmRole(ctx, UserName) == "Success")
                 {
-                    ids = crm.getSalerPersonids(ctx, Convert.ToInt64(PersonId));
-                    jsonRoot.Add("IsSuccess", "true");
-                    jsonRoot.Add("Reason", "");
-                }
-                else if (SearchType.Equals("1"))
-                {
-                    ids = crm.getProjectIds(ctx, Convert.ToInt64(PersonId));
-                    jsonRoot.Add("IsSuccess", "true");
-                    jsonRoot.Add("Reason", "");
+                    if (SearchType.Equals("0"))
+                    {
+                        ids = crm.getSalerPersonids(ctx, Convert.ToInt64(PersonId));
+                        jsonRoot.Add("IsSuccess", "true");
+                        jsonRoot.Add("Reason", "");
+                    }
+                    else if (SearchType.Equals("1"))
+                    {
+                        ids = crm.getProjectIds(ctx, Convert.ToInt64(PersonId));
+                        jsonRoot.Add("IsSuccess", "true");
+                        jsonRoot.Add("Reason", "");
+                    }
+                    else
+                    {
+                        jsonRoot.Add("IsSuccess", "false");
+                        jsonRoot.Add("Reason", "没有CRM系统权限，需要管理给权限");
+                    }
                 }
                 else
                 {
@@ -75,7 +84,60 @@ namespace Ken.CRM.CustomizeWebApi.ServicesStub
             return sContent;
         }
 
+        public string getCRMPermissionByUseName(string parameter)
+        {
+            string value = HttpContext.Current.Request.Form["Data"];
+            JObject jObject = (JObject)JsonConvert.DeserializeObject(value);
+            string DBID = jObject["DBID"].ToString();
+            string UserName = jObject["UserName"].ToString();
+            string PassWord = jObject["PassWord"].ToString();
+            string SearchType = jObject["SearchType"].ToString();
 
+            JObject jsonRoot = new JObject();
+            JArray Arr = new JArray();
+            JObject basedata = new JObject();
+            KEEPER.K3.App.CRMService crm = new KEEPER.K3.App.CRMService();
+            List<long> ids = new List<long>();
+            string sContent = "";
+
+
+            Context ctx = getContext(UserName, PassWord, 2052, DBID, "http://localhost/K3Cloud/");
+            ApiClient client = new ApiClient("http://localhost/K3Cloud/");
+            bool bLogin = client.Login(DBID, UserName, PassWord, 2052);
+            if (bLogin)//登录成功
+            {
+
+
+                if (SearchType.Equals("0"))
+                {
+                    if (getCrmRole(ctx, UserName).Equals("Success"))
+                    {
+                        jsonRoot.Add("IsSuccess", "true");
+                        jsonRoot.Add("Reason", "");
+                    }
+                    else
+                    {
+                        jsonRoot.Add("IsSuccess", "false");
+                        jsonRoot.Add("Reason", "没有CRM系统权限，需要管理给权限");
+                    }
+                }
+
+            }
+            else
+            {
+                jsonRoot.Add("IsSuccess", "false");
+                jsonRoot.Add("Reason", "登录失败");
+            }
+            if (ids != null)
+            {
+                Arr.Add(ids.ToArray());
+            }
+            jsonRoot.Add("Ids", Arr);
+            sContent = JsonConvert.SerializeObject(jsonRoot);
+
+            return sContent;
+
+        }
         public static Context getContext(string UserName, string PassWord, int ICID, string DBID, string ServerUrl)
         {
 
@@ -94,6 +156,31 @@ namespace Ken.CRM.CustomizeWebApi.ServicesStub
             return ctx;
         }
 
+       //
+        // * lc add 添加 按照CRM 角色查询是否权限，否则登录失败，提示无权限
+  
+       private string getCrmRole(Context ctx,string name)
 
+        {
+            string sql = string.Format(@"/*dialect*/ 
+select d.FNUMBER,dl.FNAME 
+from T_SEC_USER a
+inner join t_sec_userorg b on a.FUSERID = b.FUSERID
+inner join t_sec_userrolemap c on c.FENTITYID = b.FENTITYID
+inner join t_SEC_role d on d.FROLEID = c.FROLEID
+inner join t_SEC_role_l dl on dl.FROLEID = d.FROLEID
+where a.FNAME = '{0}'  and 
+( d.FNUMBER in ('CRM_BB','CRM_admin','CRM全职角色','CRM全职角色（私有）','CRM_HD','CRM_SJ','CRM_XS','CRM-XSCJ','CRM_XSHT','CRM_XSHTFJ') or dl.FDESCRIPTION like '%CRM%'  ) ", name);
+            DynamicObjectCollection col = DBUtils.ExecuteDynamicObject(ctx, sql);
+            if (col == null || col.Count<=0)
+            {
+                return "False";
+            }
+            else
+            {
+                return "Success";
+            }
+        }
+    
     }
 }
